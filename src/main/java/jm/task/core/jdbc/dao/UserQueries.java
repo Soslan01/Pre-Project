a5d0d9981083f5bfafaeb1ea69b0420861f86172
Georgii Lemko <georgylemko@yandex.ru>
2022-12-22 21:53:53 +0600

    feature/1 Косметические правки еще

--- src/main/java/jm/task/core/jdbc/dao/UserQueries.java
+++ src/main/java/jm/task/core/jdbc/dao/UserQueries.java
@@ -1,20 +1,19 @@
 package jm.task.core.jdbc.dao;
 
-public class UserQueries {
-
-    public static String createTableSql = "CREATE TABLE IF NOT EXISTS USERS("
+public interface UserQueries {
+    String createTableSql = "CREATE TABLE IF NOT EXISTS USERS("
             + "ID INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE, "
             + "USERNAME VARCHAR(26) NOT NULL, "
             + "LASTNAME VARCHAR(26) NOT NULL, "
             + "AGE INT(3) NOT NULL)";
 
-    public static String dropUsersTableSql = "DROP TABLE IF EXISTS myDBTest.USERS";
+    String dropUsersTableSql = "DROP TABLE IF EXISTS myDBTest.USERS";
 
-    public static String insertTableSql = "INSERT INTO myDBTest.USERS(USERNAME, LASTNAME, AGE) VALUES(?,?,?)";
+    String insertTableSql = "INSERT INTO myDBTest.USERS(USERNAME, LASTNAME, AGE) VALUES(?,?,?)";
 
-    public static String removeUsersSql = "DELETE FROM myDBTest.USERS WHERE id = ?";
+    String removeUsersSql = "DELETE FROM myDBTest.USERS WHERE id = ?";
 
-    public static String getUsersSql = "SELECT ID, USERNAME, LASTNAME, AGE FROM myDBTest.USERS";
+    String getUsersSql = "SELECT ID, USERNAME, LASTNAME, AGE FROM myDBTest.USERS";
 
-    public static String truncateUsersSql = "TRUNCATE TABLE USERS";
+    String truncateUsersSql = "TRUNCATE TABLE USERS";
 }
