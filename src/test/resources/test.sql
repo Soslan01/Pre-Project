7cc57fb77a91e85eee49788d74c8718d8b94f378
soslantajsaev <staysaev100398@mail.ru>
2022-12-26 15:56:16 +0300

    Spring Repack

--- src/test/resources/test.sql
+++
@@ -1,7 +0,0 @@
-insert into users (USERNAME, LASTNAME, AGE) values('Dima', 'Nochnik', 22);
-#insert into users (name, lastName, age) values('Rabbit', 'Geskure', 17);
-#insert into users (name, lastName, age) values('Leroy','Glenkins', 33);
-#update users set name = 'Alex', age = 22, lastName = 'Mafin' where id = 2;
-#select * from users;
-#select name, age from users where name = 'Steve';
-#usersdelete from users where id = 4;
